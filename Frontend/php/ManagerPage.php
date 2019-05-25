fe877321648977d0fb59dfb581453a16defea2a3
PeetTharathanakul <peet12131424@gmail.com>
2019-05-25 13:54:12 +0700

    .

---
+++ Frontend/php/ManagerPage.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Manager Page</title>
+</head>
+<body>
+	<br><br><br><br>
+	<center><h2><span class="badge badge-secondary">หน้าของผู้จัดการ</span></h2></center>
+	<div class="col-sm"><br><br><br><br>
+		<div class="container">
+			<div class="row">
+				<div class="card" style="width: 30%;">
+					<div class="card-body">
+						<div class="col-sm">
+							<center><br>
+								<button type="button" class="btn btn-dark">เพิ่มพนักงาน</button>
+							</center>
+						</div>
+					</div>
+				</div>
+				<div class="card" style="width: 30%;">
+					<div class="card-body">
+						<div class="col-sm">
+							<center><br>
+								<button type="button" class="btn btn-dark">แก้ไขพนักงาน</button>
+							</center>
+						</div>
+					</div>
+				</div>
+				<div class="card" style="width: 30%;">
+					<div class="card-body">
+						<div class="col-sm">
+							<center><br>
+								<button type="button" class="btn btn-dark">ลบพนักงาน</button>
+							</center>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br><br><br><br>
+		<div class="container">
+			<div class="row">
+				<div class="card" style="width: 30%;">
+					<div class="card-body">
+						<div class="col-sm">
+							<center><br>
+								<button type="button" class="btn btn-dark">แก้ไขสินค้า</button>
+							</center>
+						</div>
+					</div>
+				</div>
+				<div class="card" style="width: 30%;">
+					<div class="card-body">
+						<div class="col-sm">
+							<center><br>
+								<button type="button" class="btn btn-dark">ดู Dashboard</button>
+							</center>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<center><button type="button" class="btn btn-light">Log Out</button></center>
+
+</div>
+</div>
+<!-- 	PHP
+
+<button name = 'select_board_menu'onclick="window.location.href = 'login.html';">Dashboard</button>
+	<br>
+	<button name = 'select_emp_add_menu'onclick="window.location.href = 'addEmp.html';">addEmp</button>
+	<br>
+	<button name = 'select_product_menu'onclick="window.location.href = 'Product.html';">Product</button>
+	<br>
+	<button name = 'select_emp_del_menu'onclick="window.location.href = 'delEmp.html';">delEmp</button>
+	<br>
+	<button name = 'select_emp_edit_menu'onclick="window.location.href = 'editEmp.html';">editEmp</button> -->
+
+</body>
+<style>
+
+.card{
+	margin-left: auto;
+	margin-right: auto;
+	border-radius: 10pt;
+	overflow: auto;
+
+	transform: scale(0.8);
+	margin-top: -60px;
+
+}
+.card-body{
+	position: relative;
+
+}
+
+</style>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</html>
+</html>
