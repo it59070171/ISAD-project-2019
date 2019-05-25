8c2445cdda6e339e06b4d2eb091a25beece90f24
Saminya Leammookda <32773908+it59070171@users.noreply.github.com>
2019-05-25 15:57:43 +0700

    Update viewProduct2.php

--- Frontend/php/viewProduct2.php
+++ Frontend/php/viewProduct2.php
@@ -32,7 +32,7 @@
 
               Quantity: <?=$row['p_quantity']?><br>
 
-              <center><button type="button" class="btn btn-light">Back</button></center>
+              <center><button  onclick="goBack()" type="button" class="btn btn-light">Back</button></center>
 
           </div>
       </div>
@@ -58,6 +58,12 @@
 }
 
 </style>
+    
+    <script>
+function goBack() {
+  window.history.back();
+}
+</script>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
