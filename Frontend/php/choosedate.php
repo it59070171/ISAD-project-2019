de12fc679fa7ef02ac751ee8b026ef5e8444f5e0
Saminya Leammookda <32773908+it59070171@users.noreply.github.com>
2019-05-25 17:20:13 +0700

    Update choosedate.php

--- Frontend/php/choosedate.php
+++ Frontend/php/choosedate.php
@@ -24,7 +24,7 @@ exit();
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<title>Dashboard</title>
 </head>
-<body>
+<body style="background-color:#292F33;">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4"></div>
