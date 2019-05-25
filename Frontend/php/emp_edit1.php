ae82a5173a23300a5b9398a3e1afbfadaf3f4152
Saminya Leammookda <32773908+it59070171@users.noreply.github.com>
2019-05-25 20:14:20 +0700

    Update emp_edit1.php

--- Frontend/php/emp_edit1.php
+++ Frontend/php/emp_edit1.php
@@ -34,7 +34,8 @@ exit();
                 </div>
 
                 <center>
-                    <input type="submit" name="submit" value="submit">
+                    <input class="btn btn-light" type="submit" name="submit" value="submit">
+                    <button onclick="goBack()" type="button" class="btn btn-light">Back</button>
                 </center>
             </form> 
         </div>
@@ -60,6 +61,11 @@ exit();
 
 </style>
 
+  <script>
+    function goBack() {
+    window.history.back();
+}
+</script>      
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
