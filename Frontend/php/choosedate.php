db5ac224b141229dd7bedc5551581ae104113fc7
Saminya Leammookda <32773908+it59070171@users.noreply.github.com>
2019-05-25 20:12:29 +0700

    Update choosedate.php

--- Frontend/php/choosedate.php
+++ Frontend/php/choosedate.php
@@ -61,8 +61,9 @@ exit();
 					</select>
                 </div>
 
-                <center>
-                    <input type="submit" name="submit" value="submit">
+                 <center>
+                    <input class="btn btn-light" type="submit" name="submit" value="submit">
+                    <button onclick="goBack()" type="button" class="btn btn-light">Back</button>
                 </center>
             </form> 
   </div>
@@ -76,6 +77,12 @@ exit();
 
 						<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		 				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-						<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script>
+    function goBack() {
+    window.history.back();
+}
+</script>
+	</body>
 </html>
